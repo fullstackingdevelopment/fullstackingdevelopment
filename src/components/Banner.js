9d7b14ca0317f2c2fa3aa6e831f0945d3f7b8ae2
Giovanni Moscato <69764323+codingcodewhilegoofin@users.noreply.github.com>
2022-09-30 17:26:16 -0500

    Big update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,8 +11,51 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "_GMDev", "AntionioEric", "TeamMember3" ];
+  const toRotate = ["_GMDev", "AntionioEric", "TeamMember3"];
   const period = 1000;
+
+  const [windowSizeX, setwindowSizeX] = useState(window.innerWidth);
+
+  let showExtras;
+  let typingAnimation;;
+  let bannerOffset;
+  let tagline;
+  let devDescription;
+
+  if (windowSizeX[0] < 700) {
+    showExtras = false;
+    typingAnimation = 'wrap-mobile';
+    bannerOffset = 'banner-mobile';
+    tagline = 'tagline-mobile';
+    devDescription = 'p-mobile';
+  }
+  else {
+    showExtras = true;
+    typingAnimation = 'wrap';
+    bannerOffset = 'banner';
+    tagline = 'tagline';
+    devDescription = 'p';
+  }
+
+  useEffect(() => {
+
+    function updateSize() {
+      setwindowSizeX([window.innerWidth, window.innerHeight]);
+    }
+
+    window.addEventListener('resize', updateSize);
+    console.log(windowSizeX[0]);
+
+    return () => {
+      window.removeEventListener('resize', updateSize);
+    }
+  }, [windowSizeX])
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+    setwindowSizeX([window.innerWidth, window.innerHeight]);
+   
+  }, [])
 
   useEffect(() => {
     let ticker = setInterval(() => {
@@ -48,28 +91,55 @@ export const Banner = () => {
   }
 
   return (
-    <section className="banner" id="home">
+    <section className={bannerOffset} id="home">
       <Container>
         <Row className="aligh-items-center">
+
           <Col xs={12} md={6} xl={7}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Full Stack-ing Development.com</span>
-                <h1>{`Our team @`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "_GMDev", "AntionioEric", "TeamMember3" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
-              </div>}
-            </TrackVisibility>
+
+            <div >
+              <span
+                className={tagline}>
+                Full Stack-ing Development.com
+              </span>
+
+              <h1>
+               
+                <span
+                  className="txt-rotate"
+                  dataPeriod="1000"
+                  data-rotate='[ "_GMDev", "AntionioEric", "TeamMember3" ]'>
+                  <span
+                    className={typingAnimation}
+                  >
+                    {`Our team @${text}`}
+                  </span>
+                </span>
+              </h1>
+
+              <p className={devDescription}>
+                We are a team yay! 
+              </p>
+
+              <button 
+                onClick={() => console.log('connect')}>
+                  Let’s Connect <ArrowRightCircle size={25} />
+              </button>
+
+            </div>
           </Col>
+
           <Col xs={12} md={6} xl={5}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  <img src={headerImg} alt="Header Img"/>
-                </div>}
-            </TrackVisibility>
+
+            <div >
+              <img 
+                src={headerImg} 
+                alt="Header Img" 
+              />
+            </div>
+
           </Col>
+
         </Row>
       </Container>
     </section>
